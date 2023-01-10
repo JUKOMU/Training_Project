70b223d898b1d4ed16d2d45a40829eb758f74c83
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-01-10 23:17:49 +0800

    def login

--- csu/views.py
+++ csu/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from csu import models
+from django.template import RequestContext
 # Create your views here.
+
+def get(request, s):
+    return request.POST.get(s)
+def login(request):
+    return render(request, 'login.html')
